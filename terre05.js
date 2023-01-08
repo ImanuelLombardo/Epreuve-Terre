c781e6bcc1095bab8eed70e9b62ea185200ae8df
imanuel lombardo <imanuel.lombardo@hotmail.com>
2023-01-08 13:07:24 +0100

    Exercice Divisions

---
+++ terre05.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// Créez un programme qui affiche le résultat et le reste d’une division entre deux nombres.
+
+// Récupérer les 2 chiffres
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+// Calculer
+const resultat = undefined;
+
+if(num1 < num2 || num2 === 0 || process.argv.length < 4){
+  console.log("Erreur");
+} else {
+  console.log(`Resultat: ${Math.trunc(num1 / num2)}`);
+  console.log(`Reste: ${num1 % num2}`);
+};
