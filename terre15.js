9d861867bef2bdff20f9f82adbf46fb4cd2b3c66
imanuel lombardo <imanuel.lombardo@hotmail.com>
2023-01-13 13:55:40 +0100

    Exercice Terre : ok

---
+++ terre15.js
@@ -0,0 +1,13 @@
+"use strict"
+
+////////////////////////////////////////////////////
+// Créez un programme qui célèbre votre victoire.
+////////////////////////////////////////////////////
+// Exemples d’utilisation:
+// $ > ruby exo.rb
+// J’ai terminé l’Épreuve de la Terre et c’était[xxx].
+////////////////////////////////////////////////////
+
+const fileName = __filename;
+
+console.log("J'ai terminé l'Epreuve de la Terre et c'était passionnant, instructif et challengant!");
