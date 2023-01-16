ae94ce510ca056e272c4e01f082116f28d77be25
imanuel lombardo <imanuel.lombardo@hotmail.com>
2023-01-16 10:28:15 +0100

    Exercice Division modif

--- terre05.js
+++ terre05.js
@@ -9,9 +9,20 @@ const num2 = parseInt(process.argv[3]);
 // Calculer
 const resultat = undefined;
 
-if(num1 < num2 || num2 === 0 || process.argv.length < 4){
+if(num1 < num2 || num2 === 0 || length(process.argv) < 4){
   console.log("Erreur");
 } else {
   console.log(`Resultat: ${Math.trunc(num1 / num2)}`);
   console.log(`Reste: ${num1 % num2}`);
+};
+
+// Fonction
+function length(arr) {
+let count = 0;
+
+  do{
+    count++;
+  } while(arr[count] !== undefined);
+
+  return count;
 };
